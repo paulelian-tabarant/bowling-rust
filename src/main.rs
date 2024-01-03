a26dffab47579e0a68c25cd9e4c1f9e3d8238cef
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-01-03 10:09:40 +0100

    refactor, stopped while trying to move bowling to implementation dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod bowling;
+
 fn main() {
     println!("Hello, world!");
 }
--- tests/bowling_test.rs
+++ tests/bowling_test.rs
@@ -1,15 +1,21 @@
-use std::ffi::c_int;
-
 struct Bowling();
 
 impl Bowling {
-    pub(crate) fn score(&self) -> c_int {
-        todo!()
+    pub fn roll(&self, pins: i8) {
+    }
+
+    pub fn score(&self) -> i16 {
+        return 0;
     }
 }
 
 #[test]
 fn test_bowling_score_is_zero_when_bowling_object_is_created() {
     let bowling = Bowling();
+
+    for _ in 0..20 {
+        bowling.roll(0);
+    }
+
     assert_eq!(bowling.score(), 0);
 }
