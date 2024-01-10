5ecf5b54755edc16e6748149a3fc43cfd0c63817
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-01-10 12:32:49 +0100

    make spare test pass

--- src/bowling.rs
+++ src/bowling.rs
@@ -17,6 +17,9 @@ impl Bowling {
         let mut score = 0;
 
         for i in 0..self.rolls.len() {
+            if i < self.rolls.len() - 1 && self.rolls[i] + self.rolls[i + 1] == 10 {
+                score += self.rolls[i + 2];
+            }
             score += self.rolls[i];
         }
 
